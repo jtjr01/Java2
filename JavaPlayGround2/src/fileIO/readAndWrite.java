d063583262fc56fab30824a99a509befc6efd9fc
jtjr01 <jteronesjr@gmail.com>
2016-12-14 11:01:09 -0800

    Creates a file that will copy an initially populated file

---
+++ JavaPlayGround2/src/fileIO/readAndWrite.java
@@ -0,0 +1,29 @@
+package fileIO;
+
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class readAndWrite {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		java.io.File file = new java.io.File("output.txt");
+		java.io.PrintWriter output = new java.io.PrintWriter("output.txt");
+		Scanner input = new Scanner(new File ("ziptable.txt"));
+		input.useDelimiter(" ");
+		while(input.hasNext())
+		{
+			output.print(input.next());
+			output.print(" ");
+			output.print(input.next());
+			output.print(" ");
+			output.print(input.nextLine());
+			output.println(" ");
+		}
+		input.close();
+		output.close();
+
+	}
+
+}
